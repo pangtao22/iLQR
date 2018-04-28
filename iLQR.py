bug found: derivatives of continuous and discrete dynamics are different.

iLQR.py
@@ -1,11 +1,12 @@
 from pydrake.forwarddiff import jacobian
 from pydrake.all import LinearQuadraticRegulator
+from pydrake.all import DiscreteTimeLinearQuadraticRegulator
 import numpy as np
 from numpy import linalg as LA
 # Notations in this code follow "Synthesis and stabilization of complex 
 # behaviors through online trajectory optimization" by Y. Tassa and E. Todorov.
 
-class IterativeLQR:
+class DiscreteTimeIterativeLQR:
     def __init__(self, CalcF, n, m):
         self.CalcF = CalcF # dynamics
         self.n = n # number of states
@@ -25,11 +26,10 @@ class IterativeLQR:
         
         # linearize about desired fixed point
         f_x_u = jacobian(self.CalcF, np.hstack((xd, ud)))
-        A0 = f_x_u[:, 0:n]
-        B0 = f_x_u[:, n:n+m]
+        A0 = h*f_x_u[:, 0:n] + np.eye(n)
+        B0 = h*f_x_u[:, n:n+m]
         # terminal cost = 1/2*(x-xd)'*QN*(x-xd)
-        K0, QN = LinearQuadraticRegulator(A0, B0, Q, R)
-        QN *= 100000
+        K0, QN = DiscreteTimeLinearQuadraticRegulator(A0, B0, Q, R)
         
         # allocate storage for derivatives
         Qx = np.zeros((N, n))
@@ -52,8 +52,10 @@ class IterativeLQR:
 
         # simulate forward with LQR controller about x0.
         for i in range(N):
-            u[i] = -K0.dot(x[i]) + ud
-            u[i] = 0
+            u[i] = -K0.dot(x[i]-xd) + ud
+            #-----------------hack--------------------------
+#            u[i] = -0.1
+            #-----------------------------------------------
             x_u = np.hstack((x[i], u[i]))
             x[i+1] = x[i] + h*self.CalcF(x_u)
             
@@ -95,8 +97,8 @@ class IterativeLQR:
                 luu = R
                 x_u = np.hstack((x[i], u[i]))
                 f_x_u = jacobian(self.CalcF, x_u)
-                fx = f_x_u[:, 0:n]
-                fu = f_x_u[:, n:n+m]
+                fx = h*f_x_u[:, 0:n] + np.eye(n)
+                fu = h*f_x_u[:, n:n+m]
                 
                 Qx[i] = lx + fx.T.dot(Vx[i+1])
                 Qu[i] = lu + fu.T.dot(Vx[i+1])

ilqr_double_integrator.py
@@ -1,17 +1,19 @@
 import numpy as np
 from numpy import linalg as LA
 import matplotlib.pyplot as plt
-from pydrake.all import LinearQuadraticRegulator
+from pydrake.all import LinearQuadraticRegulator, DiscreteTimeLinearQuadraticRegulator
 # Notations in this code follow "Synthesis and stabilization of complex 
 # behaviors through online trajectory optimization" by Y. Tassa and E. Todorov.
 
 #%% initilization
 h = 0.01 # time step.
-N = 100 # horizon
+N = 500 # horizon
 
 # discrete double integrator dynamics
-A = np.array([[1, h], [0, 1]]) 
-B = np.array([[h**2/2], [h]])
+A0 = np.array([[1, h], [0, 1]]) 
+B0 = np.array([[h**2/2], [h]])
+A = np.array([[0, 1], [0, 0]]) 
+B = np.array([[0], [1]])
 n = 2 # number of states
 m = 1 # number of inputs
 
@@ -23,10 +25,10 @@ Quu = np.zeros((N, m, m))
 Qux = np.zeros((N, m, n))
 
 # l(x,u) = 1/2*(x'*Q*x + u'*R*u)
-Q = np.diag([1, 1]) # lqr cost
+Q = 10*np.diag([1, 1]) # lqr cost
 R = np.eye(1) # lqr cost
 # terminal cost = 1/2*x'*QN*x
-K0, QN = LinearQuadraticRegulator(A,B,Q,R)
+K0, QN = DiscreteTimeLinearQuadraticRegulator(A0,B*h,Q,R)
 
 delta_V = np.zeros(N+1)
 Vx = np.zeros((N+1, n))
@@ -42,7 +44,8 @@ x = np.zeros((N+1, n))
 x[0] = x0
 u = np.full((N, m), -1)
 for t in range(N):
-    x[t+1] = A.dot(x[t]) + B.dot(u[t])
+#    x[t+1] = A0.dot(x[t]) + B0.dot(u[t])
+    x[t+1] = x[t] + h*(A.dot(x[t]) + B.dot(u[t]))
 x_new = np.zeros((N+1, n))
 u_new = np.zeros((N, m))
 
@@ -64,8 +67,8 @@ for j in range(1):
         lu = R.dot(u[i])
         lxx = Q
         luu = R
-        fx = A
-        fu = B
+        fx = A*h + np.eye(2)
+        fu = B*h
         Qx[i] = lx + fx.T.dot(Vx[i+1])
         Qu[i] = lu + fu.T.dot(Vx[i+1])
         Qxx[i] = lxx + fx.T.dot(Vxx[i+1].dot(fx))
@@ -86,8 +89,8 @@ for j in range(1):
     x_new[0] = x[0]
     for t in range(N):
         u_new[t] = u[t] + k[t] + K[t].dot(x_new[t] - x[t])
-        x_new[t+1] = A.dot(x_new[t]) + B.dot(u_new[t])
-    
+        x_new[t+1] = x_new[t] + h*(A.dot(x_new[t]) + B.dot(u_new[t]))
+#        x_new[t+1] = A0.dot(x_new[t]) + B0.dot(u_new[t])
     
 #%% plot
 t = np.array([i*h for i in range(N+1)])

ilqr_double_integrator_new.py
@@ -1,8 +1,6 @@
 import numpy as np
-from numpy import linalg as LA
 import matplotlib.pyplot as plt
-from iLQR import IterativeLQR
-
+from iLQR import DiscreteTimeIterativeLQR
 
 #%% initilization
 # discrete double integrator dynamics
@@ -18,16 +16,15 @@ def CalcF(x_u):
     assert(x_u.size == m+n)
     x = x_u[0:n]
     u = x_u[n:n+m]    
-    x_dotdot = A.dot(x) + B.dot(u)
+    x_dot = A.dot(x) + B.dot(u)
+    return x_dot
 
-    return x_dotdot
-
-planner= IterativeLQR(CalcF, n, m)
+planner= DiscreteTimeIterativeLQR(CalcF, n, m)
 #%% iLQR
 h = 0.01 # time step.
 N = 500 # horizon
 x0 = np.array([1,0])
-Q = 1000*np.diag([1, 1]) # lqr cost
+Q = 10*np.diag([1, 1]) # lqr cost
 R = np.eye(1) # lqr cost
 xd = np.zeros(n)
 ud = np.zeros(m)

ilqr_quadrotor.py
@@ -32,7 +32,7 @@ def CalcF(x_u):
 
 #%% initilization
 h = 0.01 # time step.
-N = 1000 # horizon
+N = 500 # horizon
 
 # desired fixed point
 xd = np.array([0,1,0,0,0,0])
@@ -47,7 +47,7 @@ B0 = f_x_u[:, n:n+m]
 Q = 100*np.eye(n) # lqr cost
 R = np.eye(m) # lqr cost
 K0, S0 = LinearQuadraticRegulator(A0, B0, Q, R)
-QN = S0 # terminal cost = 1/2*(x-xd)'*QN*(x-xd)
+QN = 100*S0 # terminal cost = 1/2*(x-xd)'*QN*(x-xd)
 
 # derivatives
 Qx = np.zeros((N, n))
@@ -66,7 +66,7 @@ K = np.zeros((N, m, n))
 
 #%% iLQR
 # initial trajectory 
-x0 = np.array([0, 0.2, 0.1, 0, 0, -0.2])
+x0 = np.array([0, 0., 0.1, 0, 0, -0.2])
 x = np.zeros((N+1, n))
 u = np.zeros((N, m))
 x[0] = x0
@@ -118,8 +118,8 @@ for j in range(Ni):
         luu = R
         x_u = np.hstack((x[i], u[i]))
         f_x_u = jacobian(CalcF, x_u)
-        fx = f_x_u[:, 0:n]
-        fu = f_x_u[:, n:n+m]
+        fx = h*f_x_u[:, 0:n] + np.eye(n)
+        fu = h*f_x_u[:, n:n+m]
         
         Qx[i] = lx + fx.T.dot(Vx[i+1])
         Qu[i] = lu + fu.T.dot(Vx[i+1])