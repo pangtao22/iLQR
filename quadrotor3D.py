fix rotation matrix bug

quadrotor3D.py
@@ -57,9 +57,9 @@ def CalcRx(phi):
 def CalcRy(theta):
     c = cos(theta)
     s = sin(theta)
-    Ry = np.array([[c, 0., -s],
+    Ry = np.array([[c, 0., s],
                    [0, 1., 0],
-                   [s, 0., c]])
+                   [-s, 0., c]])
     return Ry
 
 